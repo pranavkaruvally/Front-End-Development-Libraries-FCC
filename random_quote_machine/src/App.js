29df9d862567aa31296d627f56940f928bca96cc
Pranav P <pranavsdreams@gmail.com>
2023-07-29 12:53:54 +0530

    Generates random quotes based on API

--- random_quote_machine/src/App.js
+++ random_quote_machine/src/App.js
@@ -9,12 +9,9 @@ function App() {
     "justifyContent": "center",
     "height": "100vh",
   }
-
-  const [color, updateColor] = useState("#73A857");
-  document.body.style = `background-color: ${color}`;
   return (
     <div style={appStyle}>
-      <QuoteBox appColor={color} updateColor={updateColor}/>
+      <QuoteBox/>
     </div>
   );
 }
@@ -24,22 +21,43 @@ export default App;
 class Clicker extends React.Component {
   Clicker(props) {
       this.super(props);
+      console.log("Attached");
   }
 
   updateColorFunction() {
+    const colorSet = ["#73A857", "#F39C12", "#BDBB99", "#E74C3C", "#9B59B6", "#2C3E50", "#FB6964", "#77B1A9", "#342224"];
+    let num = Math.floor(Math.random() * 10)%9;
+    this.props.updateColor(colorSet[num]);
+  }
+
+  async getQuote() {
     if (typeof this.props.updateColor === "function") {
-      const colorSet = ["#73A857", "#F39C12", "#BDBB99", "#E74C3C", "#9B59B6", "#2C3E50", "#FB6964", "#77B1A9", "#342224"];
-      let num = Math.floor(Math.random() * 10)%9;
-      this.props.updateColor(colorSet[num]);
+      const url = 'https://api.quotable.io/quotes/random';
+      const options = {
+        method: 'GET',
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        const resultJson = JSON.parse(result)[0];
+        this.props.updateQuote(resultJson['content']);
+        this.props.updateAuthor(resultJson['author']);
+        this.updateColorFunction();
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.log("This function does nothing");
     }
   }
 
   render() {
       const buttonStyle = {
           "color": "white",
-          "background-color": this.props.color,
+          "backgroundColor": this.props.color,
           "border": "none",
-          "border-radius": "3px",
+          "borderRadius": "3px",
           "font": "0.85em Arial",
           "height": "38px",
           "margin": "30px 0px 0px 0px",
@@ -48,8 +66,7 @@ class Clicker extends React.Component {
       }
 
       const clickStyle = (this.props.clickStyle == null) ? buttonStyle : Object.assign(this.props.clickStyle, buttonStyle);
-      
-      return <button onClick={() => this.updateColorFunction()} style={clickStyle}>{ this.props.content }</button>;
+      return <button onClick={() => this.getQuote()} style={clickStyle}>{ this.props.content }</button>;
   }
 }
 
@@ -75,7 +92,7 @@ class QuoteText extends React.Component {
 
   render() {
     const divStyle = {
-      "text-align": "center",
+      "textAlign": "center",
       "width": "450px",
       "height": "auto",
       "clear": "both",
@@ -83,7 +100,7 @@ class QuoteText extends React.Component {
     const textStyle = {
       "color": this.props.color,
       "font": "28px Raleway, sans-serif",
-      "font-weight": "500",
+      "fontWeight": "500",
     };
     return (
       <div className="quote-text" style={divStyle}>
@@ -93,43 +110,51 @@ class QuoteText extends React.Component {
   }
 }
 
-class QuoteBox extends React.Component {
-  QuoteBox(props) {
-    this.super(props);
-    // this.updateState = this.updateState.bind(this);
+
+function QuoteBox() {
+  const myQuote = "Life is what happens to you, While you're busy making other plans.";
+  const myAuthor = "John Lennon";
+  const quoteBoxStyle = {
+    "borderRadius": "3px",
+    "width": "450px",
+    "padding": "40px 50px",
+    "backgroundColor": "#ffffff",
   }
 
-    // updateState() {
-    //   const colorSet = ["#73A857", "#F39C12", "#BDBB99", "#E74C3C", "#9B59B6", "#2C3E50", "#FB6964", "#77B1A9", "#342224"];
-    //   let num = Math.floor((Math.random() * 10))%10;
-    //   // updateColor({appColor: colorSet[num]});
-    //   // console.log(`Changed to ${this.colorSet[num]}`);
-    // }
+  const newQuoteStyle = {
+    "position": "relative",
+    "right": -180,
+  }
 
-  render() {
-    const myQuote = "Life is what happens to you, While you're busy making other plans.";
-    const quoteBoxStyle = {
-      "border-radius": "3px",
-      "width": "450px",
-      "padding": "40px 50px",
-      "background-color": "#ffffff",
-    }
+  const colorSet = ["#73A857", "#F39C12", "#BDBB99", "#E74C3C", "#9B59B6", "#2C3E50", "#FB6964", "#77B1A9", "#342224"];
+  const num = Math.floor(Math.random()*10)%9;
+  const [appColor, updateColor] = useState(colorSet[num]);
+  const [quote, updateQuote] = useState(myQuote);
+  const [author, updateAuthor] = useState(myAuthor)
 
-    const newQuoteStyle = {
-      "position": "relative",
-      "right": -180,
-    }
+  const aStyle = {
+    "color": "white",
+    "backgroundColor": appColor,
+    "border": "none",
+    "borderRadius": "3px",
+    "font": "1.3em Arial",
+    "height": "38px",
+    "margin": "30px 0px 0px 0px",
+    "padding": "8px 18px 6px 18px",
+    "cursor": "pointer",
+  }
 
-    return (
-      <div style={quoteBoxStyle}>
-        <QuoteText quote={myQuote} color={this.props.appColor}/>
-        <QuoteAuthor id="#author" color={this.props.appColor} authorName="John Lennon" /><br/><br/>
-        <div className="buttons" style={{"display": "flex", "justifyContent": "space-between"}}>
-          <Clicker id="#tweet-quote" color={this.props.appColor} content="a" /><br/>
-          <Clicker id="#new-quote" updateColor={this.props.updateColor} clickStyle={newQuoteStyle} color={this.props.appColor} content="New quote"/><br/>
-        </div>
+  document.body.style = `background-color: ${appColor}`;
+  return (
+    <div id="quote-box" style={quoteBoxStyle}>
+      <QuoteText id="text" quote={quote} color={appColor}/>
+      <QuoteAuthor id="author" color={appColor} authorName={author} /><br/><br/>
+      <div className="buttons" style={{"display": "flex", "justifyContent": "space-between"}}>
+        <a id="tweet-quote" href="https://twitter.com/intent/tweet"><button style={aStyle}>
+          <b>t</b>
+          </button></a>
+        <Clicker id="new-quote" updateQuote={updateQuote} updateAuthor={updateAuthor} updateColor={updateColor} clickStyle={newQuoteStyle} color={appColor} content="New quote"/><br/>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
